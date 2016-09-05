ca4738d6b604cf29bdb1aea50b127767b31d0765
sicnuthinkgeo <1270896784@qq.com>
2016-09-05 20:09:13 +0800

    first commit

---
+++ MapScales/TestForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+using ThinkGeo.MapSuite.Core;
+using ThinkGeo.MapSuite.DesktopEdition;
+
+
+namespace  MapScales
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            winformsMap1.MapUnit = GeographyUnit.Meter;
+            winformsMap1.CurrentExtent = new RectangleShape(-14103004,6452457,-7162636,2497428); 
+            winformsMap1.BackgroundOverlay.BackgroundBrush = new GeoSolidBrush(GeoColor.FromArgb(255, 198, 255, 255));
+
+            //Displays the World Map Kit as a background.
+            ThinkGeo.MapSuite.DesktopEdition.WorldMapKitWmsDesktopOverlay worldMapKitDesktopOverlay = new ThinkGeo.MapSuite.DesktopEdition.WorldMapKitWmsDesktopOverlay();
+            worldMapKitDesktopOverlay.Projection = WorldMapKitProjection.SphericalMercator;
+            winformsMap1.Overlays.Add(worldMapKitDesktopOverlay);
+
+            //Sets the ZoomLevelSnapping to none to allow having the map at the exact scale desired.
+            winformsMap1.ZoomLevelSnapping = ZoomLevelSnappingMode.None;
+            //Does not allow zooming in at a scale larger than 1:1000
+            winformsMap1.MinimumScale = 1000;
+           
+            winformsMap1.Refresh();
+        }
+
+        //Updates the scroll bar and textbox after the map changed scale.
+        private void winformsMap1_CurrentScaleChanged(object sender, CurrentScaleChangedWinformsMapEventArgs e)
+        {
+            txtBoxScale.Text = String.Format("{0:0,0}", e.CurrentScale);
+            if (e.CurrentScale < vScrollBarScale.Maximum)
+            { vScrollBarScale.Value = (int)e.CurrentScale; }
+            else
+            { vScrollBarScale.Value = vScrollBarScale.Maximum; }
+
+        }
+
+        //Sets the map to the scale according to the scroll bar.
+        private void vScrollBarScale_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            winformsMap1.CurrentScale = vScrollBarScale.Value;
+            winformsMap1.Refresh();
+        }
+
+        //Updates the textbox of the scale value when scrolling the scroll bar.
+        private void vScrollBarScale_Scroll(object sender, ScrollEventArgs e)
+        {
+            txtBoxScale.Text = String.Format("{0:0,0}", e.NewValue);
+        }
+
+        //Allows to set the map to the exact desired scale in the text box
+        private void txtBoxScale_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char keyPress = e.KeyChar;
+            //Updates the scale of the map when pressing enter on the text box.
+            if (keyPress == 13)
+            {
+                txtBoxScale.Text = String.Format("{0:0,0}", txtBoxScale.Text);
+
+                int parsedScale = ParseStringToInt(txtBoxScale.Text);
+
+                if (parsedScale < vScrollBarScale.Maximum)
+                { vScrollBarScale.Value = parsedScale; }
+                else
+                { vScrollBarScale.Value = vScrollBarScale.Maximum; }
+
+                winformsMap1.CurrentScale = parsedScale;
+                winformsMap1.Refresh();
+            }
+        }
+
+        //Parses the string entered in the text box to get only the numeric value for the scale.
+        private int ParseStringToInt(string String)
+        {
+            string totalString = "";
+            int i;
+            int resultInt =  0;
+           
+            for (i = 0; i <= String.Length - 1; i += 1)
+            {
+                string subString = String.Substring(i, 1);
+                switch (subString)
+                {
+                    case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8":case "9":
+                    totalString = totalString + subString; break;
+                }
+            }
+            resultInt = System.Convert.ToInt32(totalString);
+            return resultInt;
+        }
+
+        private void winformsMap1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Displays the X and Y in screen coordinates.
+            statusStrip1.Items["toolStripStatusLabelScreen"].Text = "X:" + e.X + " Y:" + e.Y;
+
+            //Gets the PointShape in world coordinates from screen coordinates.
+            PointShape pointShape = ExtentHelper.ToWorldCoordinate(winformsMap1.CurrentExtent, new ScreenPointF(e.X, e.Y), winformsMap1.Width, winformsMap1.Height);
+
+            //Displays world coordinates.
+            statusStrip1.Items["toolStripStatusLabelWorld"].Text = "(world) X:" + Math.Round(pointShape.X, 4) + " Y:" + Math.Round(pointShape.Y, 4);
+        }
+        
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ MapScales/TestForm.designer.cs
@@ -0,0 +1,249 @@
+﻿namespace MapScales
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestForm));
+            this.btnClose = new System.Windows.Forms.Button();
+            this.ImageListToolbar = new System.Windows.Forms.ImageList(this.components);
+            this.winformsMap1 = new ThinkGeo.MapSuite.DesktopEdition.WinformsMap();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelScreen = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelWorld = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtBoxScale = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vScrollBarScale = new System.Windows.Forms.VScrollBar();
+            this.lblMinimunScale = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(734, 434);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ImageListToolbar
+            // 
+            this.ImageListToolbar.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ImageListToolbar.ImageStream")));
+            this.ImageListToolbar.TransparentColor = System.Drawing.Color.Transparent;
+            this.ImageListToolbar.Images.SetKeyName(0, "");
+            this.ImageListToolbar.Images.SetKeyName(1, "");
+            this.ImageListToolbar.Images.SetKeyName(2, "");
+            this.ImageListToolbar.Images.SetKeyName(3, "");
+            this.ImageListToolbar.Images.SetKeyName(4, "");
+            this.ImageListToolbar.Images.SetKeyName(5, "");
+            this.ImageListToolbar.Images.SetKeyName(6, "");
+            this.ImageListToolbar.Images.SetKeyName(7, "");
+            this.ImageListToolbar.Images.SetKeyName(8, "");
+            this.ImageListToolbar.Images.SetKeyName(9, "");
+            this.ImageListToolbar.Images.SetKeyName(10, "");
+            this.ImageListToolbar.Images.SetKeyName(11, "");
+            this.ImageListToolbar.Images.SetKeyName(12, "");
+            this.ImageListToolbar.Images.SetKeyName(13, "");
+            this.ImageListToolbar.Images.SetKeyName(14, "");
+            this.ImageListToolbar.Images.SetKeyName(15, "");
+            this.ImageListToolbar.Images.SetKeyName(16, "");
+            this.ImageListToolbar.Images.SetKeyName(17, "");
+            this.ImageListToolbar.Images.SetKeyName(18, "");
+            this.ImageListToolbar.Images.SetKeyName(19, "");
+            this.ImageListToolbar.Images.SetKeyName(20, "");
+            this.ImageListToolbar.Images.SetKeyName(21, "");
+            this.ImageListToolbar.Images.SetKeyName(22, "");
+            this.ImageListToolbar.Images.SetKeyName(23, "");
+            this.ImageListToolbar.Images.SetKeyName(24, "");
+            this.ImageListToolbar.Images.SetKeyName(25, "");
+            this.ImageListToolbar.Images.SetKeyName(26, "");
+            this.ImageListToolbar.Images.SetKeyName(27, "");
+            this.ImageListToolbar.Images.SetKeyName(28, "");
+            this.ImageListToolbar.Images.SetKeyName(29, "");
+            this.ImageListToolbar.Images.SetKeyName(30, "");
+            this.ImageListToolbar.Images.SetKeyName(31, "tool_icon_color_picker.png");
+            this.ImageListToolbar.Images.SetKeyName(32, "btn_layer_up.png");
+            this.ImageListToolbar.Images.SetKeyName(33, "btn_layer_down.png");
+            // 
+            // winformsMap1
+            // 
+            this.winformsMap1.BackColor = System.Drawing.Color.White;
+            this.winformsMap1.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.Default;
+            this.winformsMap1.CurrentScale = 590591790;
+            this.winformsMap1.DrawingQuality = ThinkGeo.MapSuite.Core.DrawingQuality.Default;
+            this.winformsMap1.Location = new System.Drawing.Point(143, 12);
+            this.winformsMap1.MapFocusMode = ThinkGeo.MapSuite.DesktopEdition.MapFocusMode.Default;
+            this.winformsMap1.MapResizeMode = ThinkGeo.MapSuite.Core.MapResizeMode.PreserveScale;
+            this.winformsMap1.MapUnit = ThinkGeo.MapSuite.Core.GeographyUnit.DecimalDegree;
+            this.winformsMap1.MaximumScale = 80000000000000;
+            this.winformsMap1.MinimumScale = 200;
+            this.winformsMap1.Name = "winformsMap1";
+            this.winformsMap1.Size = new System.Drawing.Size(666, 407);
+            this.winformsMap1.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.winformsMap1.TabIndex = 2;
+            this.winformsMap1.Text = "winformsMap1";
+            this.winformsMap1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            this.winformsMap1.ThreadingMode = ThinkGeo.MapSuite.DesktopEdition.MapThreadingMode.Default;
+            this.winformsMap1.ZoomLevelSnapping = ThinkGeo.MapSuite.DesktopEdition.ZoomLevelSnappingMode.Default;
+            this.winformsMap1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.winformsMap1_MouseMove);
+            this.winformsMap1.CurrentScaleChanged += new System.EventHandler<ThinkGeo.MapSuite.DesktopEdition.CurrentScaleChangedWinformsMapEventArgs>(this.winformsMap1_CurrentScaleChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelScreen,
+            this.toolStripStatusLabelWorld});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 468);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(834, 22);
+            this.statusStrip1.TabIndex = 46;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelScreen
+            // 
+            this.toolStripStatusLabelScreen.AutoSize = false;
+            this.toolStripStatusLabelScreen.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.toolStripStatusLabelScreen.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.toolStripStatusLabelScreen.Name = "toolStripStatusLabelScreen";
+            this.toolStripStatusLabelScreen.Size = new System.Drawing.Size(70, 17);
+            this.toolStripStatusLabelScreen.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabelWorld
+            // 
+            this.toolStripStatusLabelWorld.AutoSize = false;
+            this.toolStripStatusLabelWorld.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.toolStripStatusLabelWorld.Name = "toolStripStatusLabelWorld";
+            this.toolStripStatusLabelWorld.Size = new System.Drawing.Size(188, 17);
+            this.toolStripStatusLabelWorld.Text = "toolStripStatusLabel1";
+            // 
+            // txtBoxScale
+            // 
+            this.txtBoxScale.Location = new System.Drawing.Point(33, 434);
+            this.txtBoxScale.Name = "txtBoxScale";
+            this.txtBoxScale.Size = new System.Drawing.Size(92, 20);
+            this.txtBoxScale.TabIndex = 47;
+            this.txtBoxScale.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxScale_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(16, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "1:";
+            // 
+            // vScrollBarScale
+            // 
+            this.vScrollBarScale.Location = new System.Drawing.Point(19, 52);
+            this.vScrollBarScale.Maximum = 40000000;
+            this.vScrollBarScale.Minimum = 1000;
+            this.vScrollBarScale.Name = "vScrollBarScale";
+            this.vScrollBarScale.Size = new System.Drawing.Size(33, 327);
+            this.vScrollBarScale.TabIndex = 50;
+            this.vScrollBarScale.Value = 1000;
+            this.vScrollBarScale.MouseCaptureChanged += new System.EventHandler(this.vScrollBarScale_MouseCaptureChanged);
+            this.vScrollBarScale.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBarScale_Scroll);
+            // 
+            // lblMinimunScale
+            // 
+            this.lblMinimunScale.AutoSize = true;
+            this.lblMinimunScale.Location = new System.Drawing.Point(65, 52);
+            this.lblMinimunScale.Name = "lblMinimunScale";
+            this.lblMinimunScale.Size = new System.Drawing.Size(43, 13);
+            this.lblMinimunScale.TabIndex = 51;
+            this.lblMinimunScale.Text = "1:1,000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "1:40,000,000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "Scale:";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 490);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMinimunScale);
+            this.Controls.Add(this.vScrollBarScale);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxScale);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.winformsMap1);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TestForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Map Scales";
+            this.Load += new System.EventHandler(this.TestForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        internal System.Windows.Forms.ImageList ImageListToolbar;
+        private ThinkGeo.MapSuite.DesktopEdition.WinformsMap winformsMap1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelScreen;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelWorld;
+        private System.Windows.Forms.TextBox txtBoxScale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.VScrollBar vScrollBarScale;
+        private System.Windows.Forms.Label lblMinimunScale;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
